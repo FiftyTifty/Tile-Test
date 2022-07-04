5c996fb7c7580b5b184556843b0470e0e2784f37
FiftyTifty <cm@cmrms.com>
2022-07-04 20:23:59 +0100

    Upload

--- Assets/Editor/MenuCreateWorldTilemap.cs
+++ Assets/Editor/MenuCreateWorldTilemap.cs
@@ -1,6 +1,7 @@
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
+using FiftyTifty.Tilemaps;
 
 public class MenuCreateWorldTilemap : EditorWindow
 {
@@ -10,15 +11,35 @@ public class MenuCreateWorldTilemap : EditorWindow
     static void menuCreateWorldTilemap()
 	{
 
-		EditorWindow windowCreateWorldTilemap = (MenuCreateWorldTilemap) EditorWindow.GetWindow(typeof(MenuCreateWorldTilemap), true, "Create World Tilemap");
+		EditorWindow windowCreateWorldTilemap = (MenuCreateWorldTilemap) EditorWindow.GetWindow(typeof(MenuCreateWorldTilemap), true, "Create World Tilemap", true);
 		windowCreateWorldTilemap.titleContent = new GUIContent("Create World Tilemap");
 
 	}
 
-	[MenuItem("FiftyTifty/World/Create World Tilemap", false)]
+	[MenuItem("FiftyTifty/World/Create World Tilemap", true)]
 	private static bool menuCreateWorldTilemap_Check()
 	{
-		return GameObject.Find("tilemapWorld");
+		return GameObject.Find("tilemapWorld") == false;
+	}
+
+	private static void CreateWorldTilemap()
+	{
+
+		GameObject tilemapWorld = new GameObject("tilemapWorld");
+		tilemapWorld.Add
+
+	}
+
+	private void OnGUI()
+	{
+
+		EditorGUILayout.Vector2IntField("Tilemap Size", new Vector2Int (1, 1));
+
+		if (GUI.Button(new Rect(60,60, 200, 50), "Create World Tilemap"))
+		{
+			Debug.Log("Clicked");
+		}
+
 	}
 
 }
